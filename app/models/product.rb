4c284bbe18706fdd33ea5076c6a2ed0c31bee51b
saxaathlete <saxa888@gmail.com>
2010-04-12 14:18:34 +0300

    preproduction

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ActiveRecord::Base
   cattr_reader :per_page
-  @@per_page = 6
+  @@per_page = 12
 
   belongs_to :product_category
   has_attached_file :product_photo, :styles => {:small => '250x350>', :thumb => "200x200"}
